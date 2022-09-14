04ed549919ab889cc83c1390df430076f818f220
Rainer Simon <aboutgeo@gmail.com>
2022-09-14 13:36:31 +0200

    Login badge style

--- src/luna/auth/LunaAuth.svelte
+++ src/luna/auth/LunaAuth.svelte
@@ -1,5 +1,9 @@
 <script type="ts">
   import { onMount, createEventDispatcher } from 'svelte';
+  import { BarLoader } from 'svelte-loading-spinners';
+  import Icon from 'svelte-icons-pack/Icon.svelte';
+  import FiLogIn from 'svelte-icons-pack/fi/FiLogIn';
+  import FiLogOut from 'svelte-icons-pack/fi/FiLogOut';
   import { API_BASE } from '@/Config';
 
   export let tokenUrl: string;
@@ -70,15 +74,22 @@
 
 <div class="luna-auth-widget">
   {#if state === 'QUERY_TOKEN_SERVICE' || state === 'WAITING_FOR_TOKEN'}
-    {state}
+    <BarLoader size="20" color="#bcb7b4" unit="px" duration="1s" />
     <iframe 
       bind:this={iframe}
       title="iiif-auth-iframe"
       src={tokenUrl} />
   {:else if state === 'LOGGED_OUT'}
-    Not logged in <a href={loginUrl}>Login</a>
+    <a class="login" href={loginUrl}>
+      <span class="label">Login</span> 
+      <span class="icon">
+        <Icon src={FiLogIn} />
+      </span>
+    </a>
   {:else if state === 'LOGGED_IN'}
-    Logged in as: {user} <a href={logoutUrl}>Logout</a>
+    <span class="label">Logged in as:</span>
+    <strong>{user}</strong> 
+    <a href={logoutUrl} class="icon"><Icon src={FiLogOut} /></a>
   {/if}
 </div>
 
@@ -87,10 +98,44 @@
     position: absolute;
     top: 20px;
     right: 20px;
-    background-color: #fff;
-    padding: 10px 14px;
+    background-color: #574b45;
+    color: #fff;
     border-radius: 3px;
-    box-shadow: 1px 1px 12px rgba(0,0,0,0.2);
+    box-shadow: 0 0 18px rgba(0,0,0,0.2);
+    min-width: 200px;
+    height: 50px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .luna-auth-widget .label {
+    color: #e3e3e3;
+    padding: 0 8px 0 12px;
+  }
+
+  .luna-auth-widget a {
+    color: #fff;
+    text-decoration: none;
+  }
+
+  .luna-auth-widget .login {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .luna-auth-widget .login:hover {
+    background-color: #74665f;
+  }
+
+  .luna-auth-widget .icon {
+    display: inline-block;
+    padding-left: 14px;
+    padding-right: 10px;
+    padding-top: 4px;
   }
 
   .luna-auth-widget iframe {
