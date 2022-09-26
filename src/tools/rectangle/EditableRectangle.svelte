c22e7c43886ce2bef69a95e91ecf815c8be5016e
Rainer Simon <aboutgeo@gmail.com>
2022-09-26 13:18:14 +0200

    Simplified code

--- src/tools/rectangle/EditableRectangle.svelte
+++ src/tools/rectangle/EditableRectangle.svelte
@@ -9,7 +9,7 @@
   export let reverseShapeTransform: Function = null;
   export let viewportScale: number = null;
 
-  $: handleSize = 14 / viewportScale;
+  $: handleSize = 10 / viewportScale;
 </script>
 
 <Tool
@@ -31,78 +31,73 @@
 
   <g
     class="a9s-annotation selected"
-    class:hover={shape.state.isHovered}>
+    on:pointerup={release}
+    on:pointermove={pointerMove}>
+
+    <line x1={geometry.x + handleSize / 2} y1={geometry.y} x2={geometry.x + geometry.w - handleSize / 2} y2={geometry.y} />
+    <line x1={geometry.x + geometry.w} y1={geometry.y + handleSize / 2} x2={geometry.x + geometry.w} y2={geometry.y + geometry.h - handleSize / 2} />
+    <line x1={geometry.x + handleSize / 2} y1={geometry.y + geometry.h} x2={geometry.x + geometry.w - handleSize / 2} y2={geometry.y + geometry.h} />
+    <line x1={geometry.x} y1={geometry.y + handleSize / 2} x2={geometry.x} y2={geometry.y + geometry.h - handleSize / 2} />
 
     <rect 
+      class="a9s-shape-handle"
       on:pointerdown={grab(ToolHandle.SHAPE)}
-      on:pointerup={release}
-      on:pointermove={pointerMove}
-      x={geometry.x} y={geometry.y} width={geometry.w} height={geometry.h} />
+      x={geometry.x + (geometry.w - handleSize) / 2} y={geometry.y + (geometry.h - handleSize) / 2} width={handleSize} height={handleSize} />
 
     <rect 
       class="a9s-edge-handle a9s-edge-handle-top" 
       on:pointerdown={grab(ToolHandle.TOP)}
-      on:pointerup={release}
-      on:pointermove={pointerMove}
       x={geometry.x} y={geometry.y} height={1} width={geometry.w} />
 
     <rect 
       class="a9s-edge-handle a9s-edge-handle-right"
       on:pointerdown={grab(ToolHandle.RIGHT)}
-      on:pointerup={release}
-      on:pointermove={pointerMove}
       x={geometry.x + geometry.w} y={geometry.y} height={geometry.h} width={1}/>
 
     <rect 
       class="a9s-edge-handle a9s-edge-handle-bottom" 
       on:pointerdown={grab(ToolHandle.BOTTOM)}
-      on:pointerup={release}
-      on:pointermove={pointerMove}
       x={geometry.x} y={geometry.y + geometry.h} height={1} width={geometry.w} />
 
     <rect 
       class="a9s-edge-handle a9s-edge-handle-left" 
       on:pointerdown={grab(ToolHandle.LEFT)}
-      on:pointerup={release}
-      on:pointermove={pointerMove}
       x={geometry.x} y={geometry.y} height={geometry.h} width={1} />
 
     <rect 
       class="a9s-corner-handle a9s-corner-handle-topleft"
       on:pointerdown={grab(ToolHandle.TOP_LEFT)}
-      on:pointerup={release}
-      on:pointermove={pointerMove}
       x={geometry.x - handleSize / 2} y={geometry.y - handleSize / 2} height={handleSize} width={handleSize} />
 
     <rect 
       class="a9s-corner-handle a9s-corner-handle-topright"
       on:pointerdown={grab(ToolHandle.TOP_RIGHT)}
-      on:pointerup={release}
-      on:pointermove={pointerMove}
       x={geometry.x + geometry.w - handleSize / 2} y={geometry.y - handleSize / 2} height={handleSize} width={handleSize} />
     
     <rect 
       class="a9s-corner-handle a9s-corner-handle-bottomright"
       on:pointerdown={grab(ToolHandle.BOTTOM_RIGHT)}
-      on:pointerup={release}
-      on:pointermove={pointerMove}
       x={geometry.x + geometry.w - handleSize / 2} y={geometry.y + geometry.h - handleSize / 2} height={handleSize} width={handleSize} />
       
     <rect 
       class="a9s-corner-handle a9s-corner-handle-bottomleft"
       on:pointerdown={grab(ToolHandle.BOTTOM_LEFT)}
-      on:pointerup={release}
-      on:pointermove={pointerMove}
       x={geometry.x - handleSize / 2} y={geometry.y + geometry.h - handleSize / 2} height={handleSize} width={handleSize} />
   </g>
 </Tool>
 
 <style>
   rect {
-    fill:transparent;
+    fill: rgba(255, 255, 255, 0.4);
     vector-effect: non-scaling-stroke;
-    stroke: #ff0000;
-    stroke-width: 4px;
+    stroke: #06acee;
+    stroke-width: 2px;
+  }
+
+  line {
+    vector-effect: non-scaling-stroke;
+    stroke: #06acee;
+    stroke-width: 2px;
   }
 
   .a9s-edge-handle {
